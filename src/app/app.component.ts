e78f51bc0c4152e2461f4a0ab208192f3f7d13d5
THIAGO ALESSANDRO BATISTA <thiago_a_batista@estudante.sesisenai.org.br>
2023-05-04 19:49:03 -0300

    atualizacao completa pt. 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+  interface Tarefa{
+    nome: string,
+    categoria:string;
+  }
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TODO-APP';
+
+  tarefa: Tarefa = {
+    nome: "",
+    categoria:""
+  }
+
+  tarefaLista:Tarefa []=[];
+
+  cadastrarTarefa(){
+    const novaTarefa:Tarefa = {
+      nome: this.tarefa.nome,
+      categoria: this.tarefa.categoria
+    }
+
+    this.tarefa.nome=""
+    this.tarefaLista.push(novaTarefa)
+    localStorage.setItem("Tarefas: ", JSON.stringify(this.tarefaLista))
+
+  }
+
+  ngOnInit(){
+    const tarefas = localStorage.getItem("Tarefas: ")
+    if(tarefas){
+      this.tarefaLista = JSON.parse(tarefas)
+    }
+  }
+
+  salvar(tarefa: Tarefa){
+    localStorage.setItem("Tarefas: ", JSON.stringify(this.tarefaLista))
+  }
+
+  remover(indice:number){
+    this.tarefaLista.splice(indice, 1)
+    localStorage.removeItem("Tarefas: ")
+    localStorage.setItem("Tarefas: ", JSON.stringify(this.tarefaLista))
+  }
+
 }
