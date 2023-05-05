21833650eefc715f97b400a831d1fed841ffaa04
THIAGO ALESSANDRO BATISTA <thiago_a_batista@estudante.sesisenai.org.br>
2023-05-04 22:01:15 -0300

    atualizacao 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,10 @@ export class AppComponent {
   }
 
   tarefaLista:Tarefa []=[];
+  tarefasTodo: Tarefa[] =[];
+  tarefasDoing: Tarefa[] =[];
+  tarefasDone: Tarefa[] =[];
+
 
   cadastrarTarefa(){
     const novaTarefa:Tarefa = {
@@ -27,26 +31,153 @@ export class AppComponent {
     }
 
     this.tarefa.nome=""
+    if(this.tarefa.categoria=="DOING"){
+
+      this.tarefasDoing.push(novaTarefa)
+
+    }else if(this.tarefa.categoria=="DONE"){
+
+      this.tarefasDone.push(novaTarefa)
+
+    } else{
+      this.tarefasTodo.push(novaTarefa)
+    }
+
     this.tarefaLista.push(novaTarefa)
-    localStorage.setItem("Tarefas: ", JSON.stringify(this.tarefaLista))
+
+    localStorage.setItem("Tarefas to-do: ", JSON.stringify(this.tarefasTodo))
+    localStorage.setItem("Tarefas doing: ", JSON.stringify(this.tarefasDoing))
+    localStorage.setItem("Tarefas done: ", JSON.stringify(this.tarefasDone))
+
+    localStorage.setItem("Tarefas totais: ", JSON.stringify(this.tarefaLista))
 
   }
 
   ngOnInit(){
-    const tarefas = localStorage.getItem("Tarefas: ")
-    if(tarefas){
-      this.tarefaLista = JSON.parse(tarefas)
+    const tarefas1 = localStorage.getItem("Tarefas to-do: ")
+    if(tarefas1){
+      this.tarefasTodo = JSON.parse(tarefas1)
     }
+    const tarefas2 = localStorage.getItem("Tarefas doing: ")
+    if(tarefas2){
+      this.tarefasDoing = JSON.parse(tarefas2)
+    }
+    const tarefas3 = localStorage.getItem("Tarefas done: ")
+    if(tarefas1){
+      this.tarefasDone = JSON.parse(tarefas3)
+    }
+
+    const tarefa0 = localStorage.getItem("Tarefas totais: ")
+    if(tarefa0){
+      this.tarefaLista = JSON.parse(tarefa0)
+    }
+    
   }
 
-  salvar(tarefa: Tarefa){
-    localStorage.setItem("Tarefas: ", JSON.stringify(this.tarefaLista))
+  alterar(tarefa: Tarefa, indice: number){
+    localStorage.setItem("Tarefas totais: ", JSON.stringify(this.tarefaLista))
+
+    if(this.tarefa.categoria=="DOING"){
+
+      localStorage.setItem("Tarefas doing: ", JSON.stringify(this.tarefasDoing))//muda na lista que ja tava
+
+      for(let i = 0; i < this.tarefasDoing.length;i++){//passa na lista que tava
+
+        if(this.tarefasDoing[i].categoria=="DONE"){//ve se ta diferente da lista q tava e adiciona na lista certa
+
+          this.tarefasDone.push(this.tarefasDoing[i])
+
+          this.removerDoing(indice)
+
+        } else if(this.tarefasDoing[i].categoria=="TO-DO"){
+
+          this.tarefasTodo.push(this.tarefasDoing[i])
+
+          this.removerDoing(indice)
+
+        }
+      }
+
+    }else if(this.tarefa.categoria=="DONE"){
+
+      localStorage.setItem("Tarefas done: ", JSON.stringify(this.tarefasDone))
+
+      for(let i = 0; i < this.tarefasDone.length;i++){
+
+        if(this.tarefasDone[i].categoria=="DOING"){
+
+          this.tarefasDoing.push(this.tarefasDone[i])
+
+          this.removerDone(indice)
+
+        } else if(this.tarefasDone[i].categoria=="TO-DO"){
+
+          this.tarefasTodo.push(this.tarefasDone[i])
+
+          this.removerDone(indice)
+
+        }
+      }
+
+    } else{
+
+      localStorage.setItem("Tarefas to-do: ", JSON.stringify(this.tarefasTodo))
+
+      for(let i = 0; i < this.tarefasTodo.length;i++){
+
+        console.log(this.tarefasDoing)
+
+        if(this.tarefasTodo[i].categoria=="DOING"){
+
+          this.tarefasDoing.push(this.tarefasTodo[i])
+
+          this.removerTodo(indice)
+
+        } else if(this.tarefasTodo[i].categoria=="DONE"){
+
+          this.tarefasDone.push(this.tarefasTodo[i])
+
+          this.removerTodo(indice)
+
+        }
+      }
+
+    }
+    
   }
 
-  remover(indice:number){
+  removerListaTotal(indice:number){
     this.tarefaLista.splice(indice, 1)
-    localStorage.removeItem("Tarefas: ")
-    localStorage.setItem("Tarefas: ", JSON.stringify(this.tarefaLista))
+    localStorage.removeItem("Tarefas totais: ")
+    localStorage.setItem("Tarefas totais: ", JSON.stringify(this.tarefaLista))
+  }
+
+  removerTodo(indice){
+
+    this.tarefasTodo.splice(indice, 1)
+    localStorage.removeItem("Tarefas to-do: ")
+    localStorage.setItem("Tarefas to-do: ", JSON.stringify(this.tarefasTodo))
+
+    this.removerListaTotal(indice)
+
+  }
+  removerDoing(indice){
+
+    this.tarefasDoing.splice(indice, 1)
+    localStorage.removeItem("Tarefas doing: ")
+    localStorage.setItem("Tarefas doing: ", JSON.stringify(this.tarefasDoing))
+
+    this.removerListaTotal(indice)
+
+  }
+  removerDone(indice){
+
+    this.tarefasDone.splice(indice, 1)
+    localStorage.removeItem("Tarefas done: ")
+    localStorage.setItem("Tarefas done: ", JSON.stringify(this.tarefasDone))
+
+    this.removerListaTotal(indice)
+
   }
 
 }
